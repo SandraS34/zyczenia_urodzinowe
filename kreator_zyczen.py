5e70bd6572ba63d628666bf0b781e7f181d519bc
SandraS <sandra.siedlecka@opoczta.pl>
2026-05-13 21:17:11 +0200

    dodanie kodu do aktualnej daty

--- kreator_zyczen.py
+++ kreator_zyczen.py
@@ -1,20 +1,19 @@
-#wiadomosc = str(input())
-#nadawca = str(input())
+import datetime as dt
+#year = dt.date.today().year - aktualny rok
 
-print("Podaj imię solenizanta: ")
-imie=input()
+imie=input("Podaj imie solenizanta: ")
 if imie.endswith("a"): #Sprawdzam czy na końcu imienia występuje litera "a". Jeśli tak to wyświetlam tekst dla rodzaju żeńskiego
-    print(f"W którym roku urodziła się {imie}?")
-    rok = int(input())
+    #print(f"W którym roku urodziła się {imie}?")
+    rok = int(input(f"W którym roku urodziła się {imie}? "))
 else:
-    print(f"W którym roku urodził się {imie}?")
-    rok =int(input())
-wiek = 2026-rok
-print("Napisz wiadomość: ")
-wiad = input()
-print("Kto jest nadawcą? ")
-nadawca=input()
-full_text=f"{imie}! Wszystkiego najlepszego z okazji {wiek} urodzin!\n{wiad}\n{nadawca}"
+    #print(f"W którym roku urodził się {imie}?")
+    rok = int(input(f"W którym roku urodził się {imie}? "))
+wiek = dt.date.today().year - rok
+#print("Napisz wiadomość: ")
+wiad = input("Napisz wiadomość: ")
+#print("Kto jest nadawcą? ")
+nadawca = input("Kto jest nadawcą? ")
+full_text = f"{imie}! Wszystkiego najlepszego z okazji {wiek} urodzin!\n{wiad}\n{nadawca}"
 print(full_text)
 
 
